5434756c06e022a0110beadd97ec001a081e9271
yagna730 <159244501+yagna730@users.noreply.github.com>
2024-02-15 03:37:59 -0800

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    int a,b;
+    std::cout<<"Enter two value:";
+    std::cin>>a>>b;
+    std::cout<<"\nThe add. of two no. is:"<<a+b;
+    std::cout<<"\nThe sub. of two no. is:"<<a-b;
+    std::cout<<"\nThe mul. of two no. is:"<<a*b;
+    std::cout<<"\nThe div. of two no. is:"<<a/b;
+}
